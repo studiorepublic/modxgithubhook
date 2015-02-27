2e9e85946d77e246a8dfceda2d8a45c3b49f5dac
Chris Todhunter <chris@studiorepublic.com>
2015-02-27 17:24:08 +0000

    new version

--- deploy.php
+++ deploy.php
@@ -1,56 +1,21 @@
 <?php
 	/**
-	 * GIT DEPLOYMENT SCRIPT
-	 *
-	 * Used for automatically deploying websites via github or bitbucket, more deets here:
-	 *
-	 *		https://gist.github.com/1809044
-	 */
-
-	// The commands
-	$commands = array(
-/*
-		'echo $PWD',
-		'whoami',
-		'git config --list',
-		'git remote -v',
-*/
-		'git pull origin master',
-		'git status',
-/*
-		'git submodule sync',
-		'git submodule update',
-		'git submodule status',
-*/
-	);
-
-	// Run the commands for output
-	$output = '';
-	foreach($commands AS $command){
-		// Run it
-		$tmp = shell_exec($command);
-		// Output
-		$output .= "<span style=\"color: #6BE234;\">\$</span> <span style=\"color: #729FCF;\">{$command}\n</span>";
-		$output .= htmlentities(trim($tmp)) . "\n";
-	}
-
-	// Make it pretty for manual user access (and why not?)
+	* GIT DEPLOYMENT SCRIPT
+	*/
+	// Check the IP address the POST could come from
+	$ips = array('131.103.20.165','131.103.20.166','217.36.221.6');
+	if(!in_array($_SERVER['REMOTE_ADDR'],$ips)) die("Unauthorised IP address");
+	
+	//Pull from git 
+	$tmp = shell_exec('git pull origin master');
+	echo htmlentities(trim($tmp));
+	
+	//Initialise modx 
+	include("../core/model/modx/modx.class.php");
+    $modx= new modX();
+	$modx->initialize("mgr");
+	 
+	// refresh the cache
+	$modx->cacheManager->refresh();
+	
 ?>
-<!DOCTYPE HTML>
-<html lang="en-US">
-<head>
-	<meta charset="UTF-8">
-	<title>GIT DEPLOYMENT SCRIPT</title>
-</head>
-<body style="background-color: #000000; color: #FFFFFF; font-weight: bold; padding: 0 10px;">
-<pre>
- .  ____  .    ____________________________
- |/      \|   |                            |
-[| <span style="color: #FF0000;">&hearts;    &hearts;</span> |]  | Git Deployment Script v0.1 |
- |___==___|  /              &copy; oodavid 2012 |
-              |____________________________|
- 
-<?php echo $output; ?>
-</pre>
-</body>
-</html>
